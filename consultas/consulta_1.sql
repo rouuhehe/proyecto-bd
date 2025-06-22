7ccbbac87818818edd68e83af83e6fecccd8e7d3
rouuhehe <rojasevelyn88@gmail.com>
2025-06-21 23:40:16 -0500

    feat: implement 'consulta_1'

---
+++ consultas/consulta_1.sql
@@ -0,0 +1,15 @@
+
+/* CONSULTA:
+¿Qué veterinarios, agrupados por especialización, 
+han atendido la mayor cantidad de citas en el último mes?
+*/
+
+SELECT especializacion, p.nombre_completo, COUNT(*) AS cantidad_citas
+FROM veterinario
+JOIN persona p ON veterinario.dni_persona = p.dni
+JOIN cita ON veterinario.dni_persona = cita.dni_veterinario
+WHERE fecha >= CURRENT_DATE - INTERVAL '1 month'
+GROUP BY especializacion, p.nombre_completo
+ORDER BY COUNT(*) DESC
+LIMIT 10;
+
---
+++ consultas/consulta_2.sql
@@ -0,0 +1,3 @@
+/* CONSULTA 2:
+¿Cuál es el tratamiento más común a perros en los últimos seis meses?
+*/
